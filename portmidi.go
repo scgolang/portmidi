scgolang/portmidi

commit 825ee38909a5df2a856a701212ae5360515ff087

DeviceId => DeviceID

portmidi.go
@@ -26,7 +26,7 @@ import (
 )
 
 // DeviceId is a MIDI device ID.
-type DeviceId int
+type DeviceID int
 
 // DeviceInfo provides info about a MIDI device.
 type DeviceInfo struct {

commit 510a846211133831430912af7a77d4a2e77e6dad

Fix errors that where introduced with commit 825ee38909a5df2a856a701212ae5360515ff087 DeviceId => DeviceID:
Type declarations where not refactored, i changed all places that declare type DeviceID or cast to it.

portmidi.go
@@ -55,13 +55,13 @@ func Terminate() error {
 }
 
 // Returns the default input device's ID.
-func GetDefaultInputDeviceId() DeviceId {
-	return DeviceId(C.Pm_GetDefaultInputDeviceID())
+func GetDefaultInputDeviceId() DeviceID {
+	return DeviceID(C.Pm_GetDefaultInputDeviceID())
 }
 
 // Returns the default output device's ID.
-func GetDefaultOutputDeviceId() DeviceId {
-	return DeviceId(C.Pm_GetDefaultOutputDeviceID())
+func GetDefaultOutputDeviceId() DeviceID {
+	return DeviceID(C.Pm_GetDefaultOutputDeviceID())
 }
 
 // Returns the number of MIDI devices.
@@ -70,7 +70,7 @@ func CountDevices() int {
 }
 
 // Returns the device info for the device indentified with deviceId.
-func GetDeviceInfo(deviceId DeviceId) *DeviceInfo {
+func GetDeviceInfo(deviceId DeviceID) *DeviceInfo {
 	info := C.Pm_GetDeviceInfo(C.PmDeviceID(deviceId))
 	return &DeviceInfo{
 		Interface:         C.GoString(info.interf),

stream.go
@@ -54,12 +54,12 @@ type Event struct {
 
 // Stream represents a portmidi stream.
 type Stream struct {
-	deviceId DeviceId
+	deviceId DeviceID
 	pmStream *C.PmStream
 }
 
 // Initializes a new input stream.
-func NewInputStream(deviceId DeviceId, bufferSize int64) (stream *Stream, err error) {
+func NewInputStream(deviceId DeviceID, bufferSize int64) (stream *Stream, err error) {
 	var str *C.PmStream
 	errCode := C.Pm_OpenInput(
 		(*unsafe.Pointer)(unsafe.Pointer(&str)),
@@ -74,7 +74,7 @@ func NewInputStream(deviceId DeviceId, bufferSize int64) (stream *Stream, err er
 }
 
 // Initializes a new output stream.
-func NewOutputStream(deviceId DeviceId, bufferSize int64, latency int64) (stream *Stream, err error) {
+func NewOutputStream(deviceId DeviceID, bufferSize int64, latency int64) (stream *Stream, err error) {
 	var str *C.PmStream
 	errCode := C.Pm_OpenOutput(
 		(*unsafe.Pointer)(unsafe.Pointer(&str)),